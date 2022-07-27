477ae5fc7e50cb5c9837e21d2caf455e754ecc56
hyobin <lishavxn@yonsei.ac.kr>
2022-07-27 19:09:23 +0900

    typo edit

--- purple0724/purple0724/urls.py
+++ purple0724/purple0724/urls.py
@@ -1,18 +1,4 @@
-"""purple0724 URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from home.views import *
@@ -25,4 +11,4 @@ urlpatterns = [
     path('', home, name='home'),
     path('search/', search, name='search'),
     path('curation/<int:curation_id>/', c_detail, name='c_detail')
-] + static(settings.MEDIA_URL, documnet_root = settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
